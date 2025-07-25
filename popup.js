413770056328b837b2a88427fecabefed5b0353f
Arham Islam <arham.m.islam@gmail.com>
2025-07-25 03:02:02 -0400

    Update files
    - Update manifest.json
    - Add basic UI for popup
    - Add logic for Bootstrap tooltip
    - Add styling

---
+++ popup.js
@@ -0,0 +1,3 @@
+// Logic for Bootstrap tooltip to work
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
