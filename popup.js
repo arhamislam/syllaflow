44cd4d13eb545e8d5b074a229bbeaafcaef1860d
Arham Islam <arham.m.islam@gmail.com>
2025-07-27 02:57:19 -0400

    Update file structure and files
    - Switch to LF
    - Update file structure
    - Update manifest.json
    - Update popup.html, popup.js, styles.css, and README.md
    - Create background.js

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
-// Logic for Bootstrap tooltip to work
+// Logic for Bootstrap tooltips to work
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// When popup loads
+document.addEventListener('DOMContentLoaded', function() {
+    checkCurrentTab();
+});
